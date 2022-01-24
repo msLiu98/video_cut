1ff403fd08bed59200762ee1e59e11188f271936
msliu98 <mingshuoliu98@163.com>
2022-01-24 10:40:18 +0800

    add requirements and trans ts

--- cut_test.py
+++ cut_test.py
@@ -2,6 +2,13 @@ import subprocess as sp
 import math
 
 
+def trans_ts(filename, outfile):
+    cmd = f"ffmpeg -y -i {filename} -vcodec copy -acodec copy -vbsf h264_mp4toannexb {outfile}"
+    p = sp.Popen(cmd, shell=True)
+    p.wait()
+    return
+
+
 def cut_video(filename, outfile, start, length=90):
     cmd = "ffmpeg -y -i %s -ss %d -t %d -c copy %s" % (filename, start, length, outfile)
     p = sp.Popen(cmd, shell=True)
@@ -30,14 +37,16 @@ def main():
     length = 16
     length_drop = 12
     start = 1000
+    start = 0
 
+    trans_ts(filename, f'out/ts/demo.ts')
     duration = math.floor(float(get_video_duration(filename)))
     part = math.ceil(duration / length)
-    part = 30
+    # part = 30
     files = []
     for i in range(start, start + part*length, length):
-        cut_video(filename, f'out/pieces/test_{i//length}.mp4', i, length-length_drop)
-        files.append(f"file 'F:/Projects/toolkit/video_cut/out/pieces/test_{i//length}.mp4'")
+        cut_video(f'out/ts/demo.ts', f'out/pieces/test_{i//length}.ts', i, length-length_drop)
+        files.append(f"file 'F:/Projects/toolkit/video_cut/out/pieces/test_{i//length}.ts'")
     #
     filename_new = 'F:/Projects/toolkit/video_cut/file_pieces.txt'
     with open(filename_new, 'w', encoding='utf-8') as f_piece:
